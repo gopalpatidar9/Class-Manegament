7dd001431044c85f9a501e1975ce1ecf1c647c88
gopal patidar <gopalpatidara9@gmail.com.com>
2022-07-23 11:26:25 +0530

    hureku update

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,7 +2,7 @@ class Teacher < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,:confirmable
+         :recoverable, :rememberable, :validatable
 
   has_many :createclasses  
   has_one :profile   
--- config/environment.rb
+++ config/environment.rb
@@ -4,14 +4,14 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings = {
+# ActionMailer::Base.smtp_settings = {
 
-  :user_name => 'railsr209@gmail.com',
-  :password => 'ictpgyalwyqmupzc',
-  :domain => 'yourdomain.com',
-  :address => 'smtp.gmail.com',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
+#   :user_name => 'railsr209@gmail.com',
+#   :password => 'ictpgyalwyqmupzc',
+#   :domain => 'yourdomain.com',
+#   :address => 'smtp.gmail.com',
+#   :port => 587,
+#   :authentication => :plain,
+#   :enable_starttls_auto => true
 
-}
+# }
